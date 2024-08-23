1e26e5bfb443ef753249a53d3ba8b17307ab6d31
Brandon A Gomez <brandon.gomez.alex@gmail.com>
2024-08-23 11:46:14 -0600

    Create StopIfClose.cs

---
+++ StopIfClose.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyStopIfClose : MonoBehaviour
+{
+    Transform target;
+    NavMeshAgent agent;
+
+    [SerializeField] private float stoppingDis;
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>(); }
+    
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+        if (distance < stoppingDis) {
+            FaceTarget();
+            StopAgent(); }
+        else {
+            agent.isStopped = false; }
+    }
+
+    void StopAgent()
+    {
+        agent.isStopped = true;
+    }
+    
+    void FaceTarget()
+    {
+        var turnSpeed = Time.deltaTime * 5f;
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, turnSpeed);
+    }
+}
